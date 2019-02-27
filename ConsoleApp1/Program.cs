6a7abb1a3a0e311134970a3e901f4dbd12bd42ef
studenti <studenti@rosselli.loc>
2019-02-27 15:34:22 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.MaxValue;
+            Console.WriteLine(a);
+            Console.WriteLine(a + 3);
+            int b = int.MinValue;
+            Console.WriteLine(b);
+            
+            Console.ReadLine();
+            
+
+        }
+    }
+}
